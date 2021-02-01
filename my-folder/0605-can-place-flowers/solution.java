70b7aa944be0067a386ddabec006c0b09d637f6b
Joseph Lim <59989652+JosephLimWeiJie@users.noreply.github.com>
2021-02-01 13:27:18 +0000

    [LeetCode Sync] Runtime - 1 ms (98.68%), Memory - 49.1 MB (5.37%)

---
+++ my-folder/0605-can-place-flowers/solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+
+    public static boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int i = 0;
+        int count = 0;
+        
+        while (i < flowerbed.length) {
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && 
+                (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)) {
+                flowerbed[i] = 1;
+                count++;
+            }
+            i += 1;      
+        }
+        return count >= n;
+    }
+}
+

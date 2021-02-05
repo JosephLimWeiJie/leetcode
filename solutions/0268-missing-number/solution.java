3bae6e8fc131ff84d93e05b46aa0fe8d72c293a4
Joseph Lim <59989652+JosephLimWeiJie@users.noreply.github.com>
2021-02-05 04:30:20 +0000

    Sync leetcode soultions. - Missing Number - Runtime - 0 ms (100.00%), Memory - 39.5 MB (100.00%)

---
+++ solutions/0268-missing-number/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int expectedSum = nums.length * (nums.length + 1) / 2;
+        
+        for (int num : nums) {
+            expectedSum -= num;
+        }
+        
+        return expectedSum;
+    }
+}

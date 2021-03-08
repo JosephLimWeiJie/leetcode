9624d0805061900c3287c969659ab29d362aab08
Joseph Lim <59989652+JosephLimWeiJie@users.noreply.github.com>
2021-03-08 13:33:09 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 39.7 MB (100.00%)

--- my-folder/0792-binary-search/solution.java
+++ my-folder/0792-binary-search/solution.java
@@ -1,21 +1,21 @@
 class Solution {
     public int search(int[] nums, int target) {
-        return binarySearch(0, nums.length - 1, nums, target);    
+        return search(nums, target, 0, nums.length - 1);
     }
     
-    public int binarySearch(int left, int right, int[] nums, int target) {
+    public int search(int[] nums, int target, int left, int right) {
+        int mid = (left + right) / 2;
+        
         if (left > right) {
             return -1;
-        } 
+        }
         
-        int mid = (left + right) / 2;
-        int curr = nums[mid];
-        if (curr > target) {
-            return binarySearch(left, mid - 1, nums, target);
-        } else if (curr < target) {
-            return binarySearch(mid + 1, right, nums, target);
-        } else {
+        if (nums[mid] == target) {
             return mid;
+        } else if (nums[mid] < target) {
+            return search(nums, target, mid + 1, right);
+        } else {
+            return search(nums, target, left, mid - 1);
         }
     }
 }
